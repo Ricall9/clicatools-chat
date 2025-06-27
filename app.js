15db4b38d34db64bc6fddd36068ecc5dbb0e9a6d
Ricall9 <oldfarol@gmail.com>
2025-06-27 15:43:01 -0500

    app.js

--- app.js
+++ app.js
@@ -22,20 +22,17 @@ const addMsg = (text, cls) => {
   /* 👇 NUEVO: forzar target="_blank" en cada enlace que llegue */
   el.querySelectorAll('a').forEach(a => {
   const href = a.getAttribute('href');
-  
+
   if (href && href.startsWith('mailto:')) {
-    // Reemplazamos mailto por Gmail web
-    const email = href.replace('mailto:', '');
-    a.setAttribute('href', `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`);
-    a.setAttribute('target', '_blank');
-    a.setAttribute('rel', 'noopener noreferrer');
+    // No agregar target para mailto
+    a.removeAttribute('target');
+    a.removeAttribute('rel');
   } else {
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener noreferrer');
   }
 });
 
-  
   chatBox.appendChild(el);
   chatBox.scrollTop = chatBox.scrollHeight;
 };
